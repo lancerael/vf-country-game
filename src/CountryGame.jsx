a1da868776cee67f2c79eb2af45fdb1b1f1a6339
Lance Taylor <lance.taylor@vodafone.com>
2022-07-22 14:23:20 +0100

    Working game

--- src/CountryGame.jsx
+++ src/CountryGame.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo, useEffect } from 'react'
 import './CountryGame.css'
-
-//{ Germany: "Berlin", Azerbaijan: "Baku" }
 
 // type ButtonList = string[]
 // type ButtonTuple = [string, string]
@@ -11,28 +9,58 @@ import './CountryGame.css'
 //   }
 // }
 
-//const calculation = useMemo(() => expensiveCalculation(count), [count]);
-
-const makeButtonList = (data) => Object.entries(data).reduce((buttonArray, buttonTuple ) => {
+const makeButtonList = (data) => Object.entries(data).reduce((buttonArray, buttonTuple) => {
   return [...buttonArray, ...buttonTuple]
-}).sort((a,b) => 0.5 - Math.random()) 
+}).sort((a,b) => 0.5 - Math.random())
+
 
 const CountryGame = ({ data }) => {
-  const [clickedButtons, setClickedButtons] = useState([])
-
-  const buttonList = useMemo(() => makeButtonList(data), [data])
+  const [buttonList, setButtonList] = useState(useMemo(() => makeButtonList(data), [data]))
+  const [firstClick, setFirstClick] = useState('')
+  const [secondClick, setSecondClick] = useState('')
 
   const clickHandler = (button) => {
-    setClickedButtons([...clickedButtons, button])
-    console.log(clickedButtons)
+    if (firstClick === button) return
+    if (firstClick && secondClick) {
+      setFirstClick(button) 
+      setSecondClick('')
+      return
+    }
+    if (!firstClick) {  
+      setFirstClick(button)
+      return
+    }
+    setSecondClick(button)
   }
+
+  useEffect(() => {
+    if(secondClick === data[firstClick] || firstClick === data[secondClick]) {
+      setButtonList(buttonList.filter(val => ![firstClick, secondClick].includes(val)))
+    }
+  }, [secondClick])
+
+  if (!buttonList.length) return <h2>Congratulations</h2>
 
   return (
     <div>
       {
-        buttonList.map((text) => <button key={text} onClick ={() => clickHandler(text)}> {text} </button>)
+        buttonList.map((text) => {
+          let backgroundColor = ''
+          if (firstClick === text && !secondClick) {
+            backgroundColor = 'blue'
+          } else if (firstClick === text || secondClick === text) {
+            backgroundColor = 'red'
+          }
+          return <button
+            style={{backgroundColor}}
+            key={text}
+            onClick={() => clickHandler(text)}
+          >
+            {text}
+          </button>
+        })
       }
-    </div>
+    </div> 
   )
 }
 
